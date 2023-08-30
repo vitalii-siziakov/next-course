254311760c7f0813d8670856c76c85e262c3946b
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2023-08-30 22:45:06 +0300

    first commit

---
+++ components/MainContainer.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import A from './A'
+
+const MainContainer = ({ children, keywords }) => {
+  return (
+    <>
+      <Head>
+        <meta keywords={'ulbi tv nextjs' + keywords}></meta>
+        <title>Главная страница</title>
+      </Head>
+
+      <div className='navbar'>
+        <A text='Главная' href={'/'} />
+        <A text='Пользователи' href={'/users'} />
+      </div>
+      <div>{children}</div>
+      <style jsx>
+        {`
+          .navbar {
+            background: orange;
+            padding: 15px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default MainContainer
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// These styles apply to every route in the application
+import '../styles/global.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
